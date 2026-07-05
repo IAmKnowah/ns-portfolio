863038bb38401718cd485082b74f0dc1e0e7e8e8
IAmKnowah <noah.sejnoha@trojans.dsu.edu>
2026-07-05 14:07:28 -0600

    Fixed Resume Display + Tweaked AboutMe

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ export default function About() {
     <main style={{ padding: "40px" }}>
       <h1>About Me</h1>
       <p>
-        <br />I am a Dakota State University graduate majoring in Computer Game Design with an emphasis in Software Development. I have a strong foundation in computer science principles and am experienced in C and its variants, Unity, and systems programming.<br />
-        <br />In addition to technical skills, I grew up in a small town in South Dakota, in which I developed early leadership and independency skills early on. I have only further honed these skills by taking on management positions and roles in various places whether it be at the local grocery store or my own game development project I conducted in my senior year at DSU.<br />
-        <br />Finally, on top of all this, I never fail to put passion and my best work into every project I'm on. In each project, I do my best to make sure it's something that represents not only myself but also the entire team that's worked on it. This is what I believe to be my strongest skill and greatest asset to offer.<br />
+        <br />I hold a Bachelor's degree in Computer Game Design, where I focused on Software Development and broader programming practices. I have a strong foundation in computer science principles with experience mostly in C and C++, Unity (C#), as well as general understanding of both frontend and backend systems. As a quick study equipped with good foundational programming skills, I'm capable of adapting to the conditions of any programming environments.'<br />
+        <br />In addition to technical skills, I hail from a small town in South Dakota, which if you don't know, tends to be a highly competitive atmosphere since everybody knows your face and place. That being said, I developed leadership and independency skills early on, and have only further honed these skills by taking on management positions where I can in both occupation and education.<br />
+        <br />Finally, I always bring passion to my work, ensuring each iteration improves upon the last. Furthermore, once I am on a project, I make it my mission to see it through to completion. I believe this to be my greatest asset.<br />
       </p>
     </main>
   );
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,11 +7,7 @@ export default function ResumePage() {
       <h1 className="text-4xl font-bold mb-8">Resume</h1>
 
       <div className="w-full px-6 py-6">
-        <iframe
-          src={`${repo}/resume/NoahSejnoha_Resume.pdf`}
-          className="w-full h-[90vh]"
-          title="Resume"
-        />
+        <iframe src={`${repo}/resume/NoahSejnoha_Resume.pdf`} className="w-full h-[90vh]" title="Resume"/>
       </div>
     </>
   );
