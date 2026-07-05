f606c824fbcb53d6ec328c3596613e7a13f75f29
IAmKnowah <noah.sejnoha@trojans.dsu.edu>
2026-07-05 11:19:31 -0600

    URL path fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = "ns-portfolio"; 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
 	output: "export",
+	basePath: isProd ? `/${repo}` : "",
+	assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 module.exports = nextConfig;
