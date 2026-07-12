0f368665f6d4addbb7cbc4609ffd82e5c7206bd4
IAmKnowah <noah.sejnoha@trojans.dsu.edu>
2026-07-12 12:29:14 -0600

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <main style={{ padding: "40px" }}>
       <h1>About Me</h1>
       <p>
-        <br />I hold a Bachelor's degree in Computer Game Design, where I focused on Software Development and broader programming practices. I have a strong foundation in computer science principles with experience mostly in C and C++, Unity (C#), as well as general understanding of both frontend and backend systems. As a quick study equipped with good foundational programming skills, I'm capable of adapting to the conditions of any programming environments.'<br />
+        <br />I hold a Bachelor's degree in Computer Game Design, where I focused on Software Development and broader programming practices. I have a strong foundation in computer science principles with experience mostly in C and C++, Unity (C#), as well as general understanding of both frontend and backend systems. As a quick study equipped with good foundational programming skills, I'm capable of adapting to the conditions of any programming environments.<br />
         <br />In addition to technical skills, I hail from a small town in South Dakota, which if you don't know, tends to be a highly competitive atmosphere since everybody knows your face and place. That being said, I developed leadership and independency skills early on, and have only further honed these skills by taking on management positions where I can in both occupation and education.<br />
         <br />Finally, I always bring passion to my work, ensuring each iteration improves upon the last. Furthermore, once I am on a project, I make it my mission to see it through to completion. I believe this to be my greatest asset.<br />
       </p>
